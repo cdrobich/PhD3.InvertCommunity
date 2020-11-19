d0a6ec0626df0888514d76ffa4dd31a587c37cfe
Courtney Robichaud <cdrobich@gmail.com>
2020-11-18 20:21:42 -0500

    add anova

--- Scripts/Alpha diversity.R
+++ Scripts/Alpha diversity.R
@@ -4,6 +4,7 @@ library(vegan) # for NMDS
 library(ggpubr) # for arranging figures
 library(agricolae)
 library(Hmisc)
+library(car)
 
 data <- read.csv("Data/Inverts_sum_richness.csv")
 
@@ -33,6 +34,43 @@ sample.size <- inverts %>%
 #3 Uninvaded             88
 
 
+abundance.lm <- lm(Sum ~ Treatment, data = inverts)
+Anova(abundance.lm, type = "2")
+tukey1 <- HSD.test(abundance.lm, "Treatment")
+
+#Response: Sum
+#Sum Sq Df F value   Pr(>F)    
+#Treatment 20656332  2  14.154 1.29e-05 ***
+#Residuals 37214525 51 
+
+#Sum       std  r Min  Max    Q25    Q50     Q75
+#Herbicide-treated 1756.5000 1343.4022 18 149 5055 542.00 1703.5 2622.25
+#Invaded            383.3889  204.4773 18 101  757 268.75  311.0  554.25
+#Uninvaded          515.6111  585.2771 18  51 2257 174.25  256.0  568.25
+
+#Sum groups
+#Herbicide-treated 1756.5000      a
+#Uninvaded          515.6111      b
+#Invaded            383.3889      b
+
+rich.lm <- lm(Richness ~ Treatment, data = inverts)
+Anova(rich.lm, type = "2")
+tukey2 <- HSD.test(rich.lm, "Treatment")
+
+#Response: Richness
+#Sum Sq Df F value    Pr(>F)    
+#Treatment  942.81  2  10.503 0.0001514 ***
+#Residuals 2289.11 51
+
+#Richness      std  r Min Max   Q25  Q50   Q75
+#Herbicide-treated 15.33333 4.458963 18   8  23 11.25 15.5 18.75
+#Invaded           25.33333 6.782330 18  10  36 20.25 25.0 29.75
+#Uninvaded         22.22222 8.292843 18  10  37 17.00 22.5 27.75
+
+#Richness groups
+#Invaded           25.33333      a
+#Uninvaded         22.22222      a
+#Herbicide-treated 15.33333      b
 
 
 inverts <- inverts %>% 
