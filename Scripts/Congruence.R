a0848ed2cef403a255ccd82638c4ebe941b58a4b
Courtney Robichaud <cdrobich@gmail.com>
2021-01-21 14:41:47 -0500

    Updated mantel tests

--- Scripts/Congruence.R
+++ Scripts/Congruence.R
@@ -1,35 +1,64 @@
+
+# Libraries ---------------------------------------------------------------
+
+
 library(tidyverse) # for figures, pipes etc.
 library(vegan) # for NMDS
-library(ggpubr) # for arranging figures
 
 
-### Mantel test ####
+# Load data ---------------------------------------------------------------
 
-# Compare juvenille inverts to emerging inverts in each community
+# deleted INOW and UNOW so same number of rows (25) in both 
 
-benthic <- read.csv("Data/benthics_rares_rel.csv") # veg QCC samples, rares (<=2) removed and rel. by col max
-emerge <- read.csv("Data/inverts_rare_rel_2018.csv") # 2018 emergent community, rares and rel. same as above
+benthic <- read.csv("Data/benthic_congruence.csv") # should be only flying taxa
+emerge <- read.csv("Data/emerging_congruence.csv") # 2018 emergent community, only flying
 
 str(benthic)
 str(emerge)
+
+# just the environmental variables
+benthic.env <- benthic %>% select(Site.ID:Sample.Type)
+emerge.env <- emerge %>% select(Site:Habitat)
+
+benthic.env$ID <- rownames(benthic.env)
+emerge.env$ID <- rownames(emerge.env)
+
+# Just the species
+benthic.spp <- benthic %>% select(Lampyridae:Hydroptilidae)
+emerge.spp <- emerge %>% select(Phalacridae:Crambidae)
+
+
+# make them presence/absence
+
+benthic.spp[benthic.spp > 0] <- 1
+emerge.spp[emerge.spp >0] <- 1
+
+benthic.spp$ID <- rownames(benthic.spp)
+emerge.spp$ID <- rownames(emerge.spp)
+
+# put env variables back in
+
+benthic.pa <- full_join(benthic.env, benthic.spp)
+emerge.pa <- full_join(emerge.env, emerge.spp)
 
 ##### Mantel tests ######
 ?mantel
 
 ## uninvaded Mantel first first
 
-uninvaded.b <- subset(benthic,
+uninvaded.b <- subset(benthic.pa,
                       Habitat =="Uninvaded")
 
-uninvaded.b.inverts <- uninvaded.b[,4:25]
-uninvaded.b.env <- uninvaded.b[,1:3]
+uninvaded.b.inverts <- uninvaded.b %>% select(Lampyridae:Hydroptilidae)
 
 
-uninvaded.e <- subset(emerge,
-                      Habitat =="Uninvaded")
 
-uninvaded.e.inverts <- uninvaded.e[,4:93]
-uninvaded.e.env <- uninvaded.e[,1:3]
+uninvaded.e <- subset(emerge.pa,
+                      Treatment =="Uninvaded")
+
+uninvaded.e.inverts <- uninvaded.e %>% select(Phalacridae:Crambidae)
+
+
 
 
 # B-C dissimilarity matrix 
@@ -41,15 +70,33 @@ uninvaded.emerge.bc <- vegdist(uninvaded.e.inverts, method = "bray")
                             method = "pearson",
                             permutations = 999))
 
+# Jaccard
 
-#Mantel statistic r: 0.07563 
-#Significance: 0.346 
+#Mantel statistic r: 0.09216 
+#Significance: 0.298 
 
-# Upper quantiles of permutations (null model):
-#  90%   95%  97.5%   99% 
-#  0.241  0.341 0.406 0.456 
-# Permutation: free
-# Number of permutations: 999
+#Upper quantiles of permutations (null model):
+#  90%   95% 97.5%   99% 
+#  0.255 0.321 0.383 0.472 
+#Permutation: free
+#Number of permutations: 999
+
+
+# Bray Curtis
+
+#Mantel statistic r: 0.1223 
+#Significance: 0.248 
+
+#Upper quantiles of permutations (null model):
+#  90%   95% 97.5%   99% 
+#  0.244 0.306 0.380 0.425 
+#Permutation: free
+#Number of permutations: 999
+
+
+
+
+
 
 
 ### invaded ###
@@ -59,15 +106,14 @@ uninvaded.emerge.bc <- vegdist(uninvaded.e.inverts, method = "bray")
 invaded.b <- subset(benthic,
                       Habitat =="Invaded")
 
-invaded.b.inverts <- invaded.b[,4:25]
-invaded.b.env <- invaded.b[,1:3]
+invaded.b.inverts <- invaded.b %>% select(Lampyridae:Hydroptilidae)
 
 
 invaded.e <- subset(emerge,
-                      Habitat =="Invaded")
+                      Treatment =="Invaded")
 
-invaded.e.inverts <- invaded.e[,4:93]
-invaded.e.env <- invaded.e[,1:3]
+invaded.e.inverts <- invaded.e %>% select(Phalacridae:Crambidae)
+
 
 
 # B-C dissimilarity matrix 
@@ -79,36 +125,52 @@ invaded.emerge.bc <- vegdist(invaded.e.inverts, method = "bray")
                             method = "pearson",
                             permutations = 999))
 
-#Mantel statistic based on Pearson's product-moment correlation 
-#Mantel statistic r: -0.3187 
-#      Significance: 0.925 
+# Jaccard
+
+#Mantel statistic r: -0.0658 
+#Significance: 0.61 
 
 #Upper quantiles of permutations (null model):
 #  90%   95% 97.5%   99% 
-#0.287 0.350 0.393 0.454 
+#  0.203 0.259 0.289 0.359 
 #Permutation: free
 #Number of permutations: 999
 
 
-###  Restored ###
+# Bray-Curtis
+
+#Mantel statistic r: -0.04851 
+#Significance: 0.561 
+
+#Upper quantiles of permutations (null model):
+#  90%   95% 97.5%   99% 
+#  0.259 0.303 0.340 0.372 
+#Permutation: free
+#Number of permutations: 999
+
+
+
+
+
+
+### Treated ###
 
 ## Restored Mantel first first
 
 restored.b <- subset(benthic,
-                    Habitat =="Restored")
+                    Habitat =="Treated")
 
-Restored.b.inverts <- restored.b[,4:25]
-Restored.b.env <- restored.b[,1:3]
+Restored.b.inverts <- restored.b %>% select(Lampyridae:Hydroptilidae)
 
 
 Restored.e <- subset(emerge,
-                    Habitat =="Restored")
+                    Treatment =="Treated")
 
-Restored.e.inverts <- Restored.e[,4:93]
-Restored.e.env <- Restored.e[,1:3]
+Restored.e.inverts <- Restored.e %>% select(Phalacridae:Crambidae)
 
 
-# B-C dissimilarity matrix 
+
+# dissimilarity matrix 
 
 Restored.benthic.bc <- vegdist(Restored.b.inverts, method = "bray")
 Restored.emerge.bc <- vegdist(Restored.e.inverts, method = "bray")
@@ -116,25 +178,28 @@ Restored.emerge.bc <- vegdist(Restored.e.inverts, method = "bray")
 (Restored.mantel <- mantel(Restored.emerge.bc, Restored.benthic.bc,
                           method = "pearson",
                           permutations = 999))
+# Jaccard Distance
 
-#Mantel statistic based on Pearson's product-moment correlation 
-#Mantel statistic r: 0.1133 
-#      Significance: 0.319 
+#Mantel statistic r: -0.3519 
+#Significance: 0.925 
 
 #Upper quantiles of permutations (null model):
 #  90%   95% 97.5%   99% 
-# 0.260 0.322 0.362 0.398 
+#  0.439 0.568 0.644 0.687 
 #Permutation: free
 #Number of permutations: 999
 
-############ Procrustes #############
 
-(res.pro <- procrustes(Restored.emerge.bc, Restored.benthic.bc))
+# Bray Curtis
 
-summary(res.pro)
-plot(res.pro)
+#Mantel statistic r: -0.3498 
+#Significance: 0.936 
 
-
+#Upper quantiles of permutations (null model):
+#  90%   95% 97.5%   99% 
+#  0.567 0.666 0.743 0.788 
+#Permutation: free
+#Number of permutations: 999
 
 
 
